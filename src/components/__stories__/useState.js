e51ff6d2ed058c609695020d7f75346bed3acf69
Hugo Raynal <hugo.raynal@sterblue.com>
2019-03-04 18:47:50 +0100

    trigger

--- src/components/__stories__/useState.js
+++ src/components/__stories__/useState.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 
-import { WithoutUseState } from "../useState";
+import { WithoutUseState } from "../useState/simple";
 
 storiesOf("useState", module)
   .add("Without", () => <WithoutUseState />)
